11177cfa0e0843be0fffabdd9a70c0ce8b44d2c8
Chung Kai Hsieh <chungkaihsieh@gmail.com>
2018-05-15 16:17:25 +0800

    fix data preprocessing error

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pandas.tseries.offsets import *
 import numpy as np
 from sklearn import preprocessing
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 import time 
 
 import keras
@@ -20,31 +20,35 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt  
 
 
-def read_data(file_name):
-	df = pd.read_csv(file_name, skipinitialspace=True)
-	df.dropna(how='any',inplace=True)
+def read_data(file_name, first_time):
+	df_name = 'etf_df.csv'
 
-	# #change columns name
-	cols = ['id','date','name','open','high','low','close','volume']
-	df.columns = cols
+	if first_time :
+		df = pd.read_csv(file_name, skipinitialspace=True)
+		df.dropna(how='any',inplace=True)
 
-	# #reorder columns
-	# cols = ['date','id','open','close','high', 'low','volume','name']
-	# df = df[cols]
+		# #change columns name
+		cols = ['id','date','name','open','high','low','close','volume']
+		df.columns = cols
 
-	#Turn ['date'] type from string('20180130') to date(2018-01-30)
-	df['date'] = pd.to_datetime(df['date'], format='%Y%m%d', errors='ignore')
-	
-	#Turn feature type to float32
-	for c in cols:
-		if c not in ['name','date']:
-			df[c] = df[c].apply(lambda x: np.float32(x.replace(',','')) if isinstance(df.iloc[0][c], basestring) else np.float32(x))
+		#Turn ['date'] type from string('20180130') to date(2018-01-30)
+		df['date'] = pd.to_datetime(df['date'], format='%Y%m%d', errors='ignore')
+		
+		#Turn feature type to float32
+		for c in cols:
+			if c not in ['name','date']:
+				df[c] = df[c].apply(lambda x: np.float32(x.replace(',','')) if isinstance(df.iloc[0][c], basestring) else np.float32(x))
 
+		# add up/down of close value as feature
+		df['ud']  = np.sign(df['close'].subtract(pd.concat([pd.Series(0), df['close'][0:-1]],ignore_index=True)))
 
-	# add up/down of close value as feature
-	df['ud']  = np.sign(df['close'].subtract(pd.concat([pd.Series(0), df['close'][0:-1]],ignore_index=True)))
+		#save to csv
+		df.to_csv(df_name)
 
-
+	else:
+		df = pd.read_csv(df_name)
+		#Turn ['date'] type from string('2018-01-30') to date(2018-01-30)
+		df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d', errors='ignore')
 	return df
 
 
@@ -54,7 +58,7 @@ def data_helper(df, time_frame, train_interval, test_interval, day_offset):
 	number_features = len(feature_cols)
 	# extract id
  	id_unique = df['id'].unique()
-
+ 	# print('id_unique' + str(id_unique))
 
 	#--------------------------#
 	# Training data preprocess #
@@ -77,7 +81,7 @@ def data_helper(df, time_frame, train_interval, test_interval, day_offset):
 
 
 	train_result = np.array(train_result).astype(dtype='float32')
-	x_train_close = train_result[:,:-(day_offset)-1] #Extract every time_frame -(day_offset + last day) as feature
+	x_train = train_result[:,:-(day_offset)-1] #Extract every time_frame -(day_offset + last day) as feature
 	y_train_close = train_result[:,-1][:,-1] # Extract the last one time_frame close(收盤價) value as label
 	y_train_ud = train_result[:,-1][:,-2] # Extract the last one time_frame ud(收盤價漲跌) value as label
 
@@ -102,12 +106,13 @@ def data_helper(df, time_frame, train_interval, test_interval, day_offset):
 
 
 	test_result = np.array(test_result).astype(dtype='float32')
-	x_test_close = test_result[:,:-(day_offset)-1] #Extract every time_frame -(day_offset + last day) as feature
+	x_test = test_result[:,:-(day_offset)-1] #Extract every time_frame -(day_offset + last day) as feature
 	y_test_close = test_result[:,-1][:,-1] # Extract the last one time_frame close(收盤價) value as label
 	y_test_ud = test_result[:,-1][:,-2] # Extract the last one time_frame ud(收盤價漲跌) value as label
 
 
-	return [x_train_close, y_train_close, y_train_ud, x_test_close, y_test_close, y_test_ud]
+	# print('id_unique' + str(np.unique(x_train[:,0][:,0])))
+	return [x_train, y_train_close, y_train_ud, x_test, y_test_close, y_test_ud]
 
 
 
@@ -128,21 +133,16 @@ def normalize(df):
 
 def denormalize(col, df, norm_value):
     original_value = df[col].values.reshape(-1,1)
-    norm_value = norm_value.reshape(-1,1)
-    
-    min_max_scaler = preprocessing.MinMaxScaler()
+    norm_value = norm_value.reshape(-1,1)    
+    min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1), copy=True)
     min_max_scaler.fit_transform(original_value)
     denorm_value = min_max_scaler.inverse_transform(norm_value)
     
     return denorm_value
 
 
-def write_csv(x_test, pred_ud, pred_close, file_name):
-	print('x_test.shape:{0}, pred_ud.shape:{1}, pred_close.shape:{2}'.format(x_test.shape, pred_ud.shape, pred_close.shape))
-	# print(x_test[:,0][:,0], x_test[:,0][:,0].shape)
-	# print(pred_ud[0::5], pred_ud[0::5].shape)
-
-	pred_id = x_test[:,0][:,0][::5].astype(dtype=int)
+def write_csv(id, pred_ud, pred_close, file_name):
+	pred_id = np.rint(id).astype(dtype=int).flatten()
 	pred_ud = pred_ud.flatten()
 	pred_close = pred_close.flatten()
 
@@ -167,13 +167,12 @@ def write_csv(x_test, pred_ud, pred_close, file_name):
 def build_model_close(input_length, input_dim):
     d = 0.2
     model = Sequential()
-    model.add(LSTM(128, input_shape=(input_length, input_dim), return_sequences=True))
+    model.add(LSTM(60, input_shape=(input_length, input_dim), return_sequences=False))
     model.add(Dropout(d))
-    model.add(LSTM(128, input_shape=(input_length, input_dim), return_sequences=False))
-    model.add(Dropout(d))
-    model.add(Dense(32,kernel_initializer="uniform",activation='linear'))
-    model.add(Dense(16,kernel_initializer="uniform",activation='linear'))
-    model.add(Dense(1,kernel_initializer="uniform",activation='sigmoid'))
+    # model.add(LSTM(256, input_shape=(input_length, input_dim), return_sequences=False))
+    # model.add(Dropout(d))
+    model.add(Dense(16,kernel_initializer="uniform",activation='relu'))
+    model.add(Dense(1,kernel_initializer="uniform",activation='linear'))
     model.compile(loss='mse',optimizer='adam', metrics=['accuracy'])
     return model
 
@@ -184,100 +183,95 @@ def build_model_ud(input_length, input_dim):
     model = Sequential()
     model.add(LSTM(32, input_shape=(input_length, input_dim), return_sequences=False))
     model.add(Dropout(d))
-    # model.add(LSTM(32, input_shape=(input_length, input_dim), return_sequences=False))
-    model.add(Dropout(d))
-    model.add(Dense(32,activation='relu'))
-    model.add(Dropout(d))
-    model.add(Dense(8,activation='relu'))
+    model.add(Dense(16,activation='relu'))
     model.add(Dropout(d)) 
-    model.add(Dense(2, activation='softmax'))
-    model.compile(loss='binary_crossentropy',optimizer='adam', metrics=['accuracy'])
+    model.add(Dense(3, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
     return model
 
 
 
 if __name__ == "__main__":
 	t_start = time.time()
-	stocks_df = read_data('../TBrain_Round2_DataSet_20180504/tetfp.csv')
+	stocks_df = read_data('../TBrain_Round2_DataSet_20180511/tetfp.csv', first_time=False)
 	print('[read_data] costs:' + str(time.time() - t_start) + 'secs')
 	t_start = time.time()
 	stocks_df_normalize = normalize(stocks_df)
-
 	print('[normalize] costs:' + str(time.time() - t_start) + 'secs')
 	t_start = time.time()
 
 	# 以time_frame天為一區間進行股價預測(i.e 觀察10天股價, 預測第11天)
-	# 觀察1~5天 預測 5天(day_offset)後 (i.e 第11天) 的股價
+	# 觀察1~10天(1 ~ time_feame-day_offset) 預測 5天(day_offset)後 (i.e 第16天) 的股價
 	time_frame = 15
 	day_offset = 5
 	#train_interval = [train_first_day,train_last_day] 
 	#(cautions): train_first_day - time_frame >= data first day
-	# train_interval = ['20130201','20180504']
-	train_interval = ['20180101','20180427']
-	test_interval = ['20180430','20180504']
+
+	train_interval = ['20180101','20180504']
+	test_interval = ['20180507','20180511']
 	x_train, y_train_close, y_train_ud, x_test, y_test_close, y_test_ud = data_helper(stocks_df_normalize, time_frame, train_interval, test_interval, day_offset)
 
 	print('[data helper] costs:' + str(time.time() - t_start) + 'secs')
 	t_start = time.time()
 
-	# print(y_test_ud)
-
-	# #------------------------#
-	# # Close value prediction #
-	# #------------------------#
-
-	# # time_frame days、6 dims
-	# model_close = build_model_close( time_frame - day_offset, 7 )
-	# model_close.fit( x_train, y_train_close, batch_size=32, epochs=10, validation_split=0.1, shuffle=True, verbose=1)
-
-	# # Use trained model to predict
-	# pred_close = model_close.predict(x_test)
-	# # denormalize
-	# denorm_pred_close = denormalize('close', stocks_df, pred_close)
-	# denorm_ytest_close = denormalize('close', stocks_df, y_test_close)
-
-
-
 
 	#------------------------#
+	# Close value prediction #
+	#------------------------#
+
+	# sequence length(觀察的天數~= time_frame - day_offset) ; feature length (7 dims)
+	model_close = build_model_close( time_frame - day_offset, 7 )
+	earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=7)
+	model_close.fit( x_train, y_train_close, batch_size=16, epochs=1, validation_split=0.1, shuffle=True, verbose=1, callbacks=[earlyStopping])
+
+	# Use trained model to predict
+	pred_close = model_close.predict(x_test)
+	# denormalize
+	denorm_pred_close = denormalize('close', stocks_df, pred_close)
+	denorm_ytest_close = denormalize('close', stocks_df, y_test_close)
+
+
+
+
+	# ------------------------#
 	#   ud value prediction  #
-	#------------------------#
+	# ------------------------#
 	# convert integers to dummy variables (i.e. one hot encoded)
-	y_train_ud[y_train_ud == -1] = 0.0 # turn stocks drop(-1) to value(0)
+	y_train_ud+= 1 # [drop, balance, up] turn [-1, 0, 1] to [0, 1, 2]
 	y_train_categorical_ud = np_utils.to_categorical(y_train_ud)
-	print(y_train_categorical_ud)
-	# time_frame days、6 dims
+
+	# sequence length(觀察的天數~= time_frame - day_offset) ; feature length (7 dims)
 	model_ud = build_model_ud( time_frame - day_offset, 7 )
-	model_ud.fit( x_train, y_train_categorical_ud, batch_size=32, epochs=500, validation_split=0.1, shuffle=False, verbose=1)
+	earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=7)
+	model_ud.fit( x_train, y_train_categorical_ud, batch_size=16, epochs=1, validation_split=0.1, shuffle=True, verbose=1, callbacks=[earlyStopping])
 
 	# Use trained model to predict
 	pred_categorical_ud = model_ud.predict(x_test)
 	#inverse value from to_categorical
 	pred_ud = np.argmax(pred_categorical_ud, axis= 1)
-	y_train_ud[y_train_ud == 0] = -1 # turn value(0) to stocks drop(-1)
-
-	# denormalize
-	denorm_pred_ud = denormalize('ud', stocks_df, pred_ud)
-	denorm_ytest_ud = denormalize('ud', stocks_df, y_test_ud)
+	pred_ud -= 1 # [drop, balance, up] turn [0, 1, 2] to [-1, 0, 1]
 
 
-	# #------------------------#
-	# #      write to csv      #
-	# #------------------------#
-	# write_csv(x_test, denorm_pred_ud, denorm_pred_close, 'submission.csv')
 
 
-	# #------------------------#
-	# #         matplot        #
-	# #------------------------#
-	# #matplotlib inline  
-	# plt.plot(denorm_pred,color='red', label='Prediction')
-	# plt.plot(denorm_ytest,color='blue', label='Answer')
-	# plt.legend(loc='best')
-	# plt.show()
+	#------------------------#
+	#      write to csv      #
+	#------------------------#
+	denorm_id = denormalize('id', stocks_df, x_test[:,0][:,0][::5])
+	write_csv(denorm_id, pred_ud, denorm_pred_close, 'submission.csv')
 
-	# #matplotlib inline  
-	# plt.plot(denorm_pred_ud,color='red', label='Prediction')
-	# plt.plot(denorm_ytest_ud,color='blue', label='Answer')
-	# plt.legend(loc='best')
-	# plt.show()
+
+	# ------------------------#
+	#         matplot        #
+	# ------------------------#
+	#matplotlib inline  
+	plt.plot(denorm_pred_close,color='red', label='Prediction')
+	plt.plot(denorm_ytest_close,color='blue', label='Answer')
+	plt.legend(loc='best')
+	plt.show()
+
+	#matplotlib inline  
+	plt.plot(pred_ud,color='red', label='Prediction')
+	plt.plot(y_test_ud,color='blue', label='Answer')
+	plt.legend(loc='best')
+	plt.show()
